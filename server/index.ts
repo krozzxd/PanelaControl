a2847a9ca1c6f187955108f664a8299361851fae
Luiz <tluizgamer12@gmail.com>
2025-02-20 18:52:31 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,10 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+connectionString: 'postgresql://postgres:LXGGEnYVIfbQMRwKpsQENQIiMkbPzSFC@yamanote.proxy.rlwy.net:44192/ferrovia',
+  ssl: {
+    rejectUnauthorized: false
+
 log("Iniciando servidor Express...", "express");
 
 const app = express();
